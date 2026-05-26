83c1a1edcd1be46cc027b6ce3a359d3b9afd673f
correalm <luismigueldasilva.99@gmail.com>
2026-05-26 08:51:26 -0300

    first implementation of part II

--- day_2/day_2.rb
+++ day_2/day_2.rb
@@ -1,18 +1,32 @@
 module Day2
   extend self
 
-  def call
+  def part_one
     file = read_file
-
-    lines = read_lines_from(file)
-    parsed_lines = lines.map{ |line| parse(line) }
+    parsed_lines = parse_lines_from(file)
 
     result = parsed_lines.map do |dimension|
       calculate_total_area(calculate_surface_area_of(dimension),
                            find_small_side_of(dimension))
     end
 
-    p result.sum
+    p "Part I: #{result.sum}"
+
+    file.close
+  end
+
+  def part_two
+    file = read_file
+    parsed_lines = parse_lines_from(file)
+
+    result = parsed_lines.map do |dimension|
+      cubic_volume = calculate_cubic_volume_of(dimension)
+      smallest_perimeter = find_smallest_perimeter_of(dimension)
+
+      cubic_volume + smallest_perimeter
+    end
+
+    p "Part II: #{result.sum}"
 
     file.close
   end
@@ -23,6 +37,10 @@ module Day2
 
   def calculate_total_area(surface_area, extra)
     surface_area + extra
+  end
+
+  def calculate_cubic_volume_of(dimension)
+    dimension.length * dimension.width * dimension.height
   end
 
   def calculate_surface_area_of(dimension)
@@ -39,6 +57,23 @@ module Day2
     [(l*w), (w*h), (h*l)].min
   end
 
+  def find_smallest_perimeter_of(dimension)
+    l = dimension.length
+    w = dimension.width
+    h = dimension.height
+
+    [(2*l + 2*w), (2*w + 2*h), (2*h + 2*l)].min
+  end
+
+  def parse_lines_from(file)
+    lines = read_lines_from(file)
+    lines.map{ |line| parse(line) }
+  end
+
+  def read_lines_from(file)
+    file.each_line(chomp: true)
+  end
+
   def parse(line)
     # each line is lxwxh (10x1x1)
     values = line.split("x")
@@ -49,10 +84,7 @@ module Day2
   def read_file
     File.new('puzzle.txt')
   end
-
-  def read_lines_from(file)
-    file.each_line(chomp: true)
-  end
 end
 
-Day2.call
+Day2.part_one
+Day2.part_two
