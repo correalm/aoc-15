2957f31c4200a33e024b4f16c13480d6a83f14a8
correalm <luismigueldasilva.99@gmail.com>
2026-06-18 21:21:02 -0300

    refac day 4 part one

--- day_4/day_4.rb
+++ day_4/day_4.rb
@@ -7,32 +7,30 @@ module Day4
 
   extend Logger
 
+  HEAD_MATCHER = '00000'
+
   def part_one
     input = File.open(File.expand_path("puzzle.txt", __dir__)).readline.strip
-    # hash = Digest::MD5.file().hexdigest
-    #File.expand_path("puzzle.txt", __dir__)
 
     ok = false
-    counter = 0
+    salt = 0
     result = nil
 
     while !ok
-      result = Digest::MD5.hexdigest input + counter.to_s
+      result = Digest::MD5.hexdigest input + salt.to_s
 
-      ok = is_ok(result)
-      counter += 1 unless ok
+      ok = check_head_of result
+      salt += 1 unless ok
     end
 
-
-    p "result #{result} :: #{counter}"
+    log(day: 4, part: 1, result: salt)
   end
 
   def part_two
   end
 
-  def is_ok(result)
-    p "Slice: #{result.slice(0, 6)}"
-    result.slice(0, 5) == '00000'
+  def check_head_of(result)
+    result.slice(0, 5) == HEAD_MATCHER
   end
 end
 
