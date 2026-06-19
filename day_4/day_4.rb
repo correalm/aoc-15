390e1e966a1bfec28b3a28ff0fc02481abb75507
correalm <luismigueldasilva.99@gmail.com>
2026-06-18 21:37:46 -0300

    tidy and solve part II
    I'm still think that this is a "bad" implementation. It looks wierd when
    we have no context about what we are doing. I need to make it more clear

--- day_4/day_4.rb
+++ day_4/day_4.rb
@@ -7,30 +7,52 @@ module Day4
 
   extend Logger
 
-  HEAD_MATCHER = '00000'
+  HEAD_MATCHER = '0'
 
   def part_one
-    input = File.open(File.expand_path("puzzle.txt", __dir__)).readline.strip
+    input = read_input
 
-    ok = false
-    salt = 0
-    result = nil
-
-    while !ok
-      result = Digest::MD5.hexdigest input + salt.to_s
-
-      ok = check_head_of result
-      salt += 1 unless ok
-    end
+    salt = find_salt_for(input: input, leading_zeroes: 5)
 
     log(day: 4, part: 1, result: salt)
   end
 
   def part_two
+    input = read_input
+
+    salt = find_salt_for(input: input, leading_zeroes: 6)
+
+    log(day: 4, part: 2, result: salt)
   end
 
-  def check_head_of(result)
-    result.slice(0, 5) == HEAD_MATCHER
+  private
+
+  def find_salt_for(input:, leading_zeroes:)
+    ok = false
+    salt = 0
+
+    while !ok
+      ok = check_head_of(hash: hash(input, salt),
+                         leading_zeroes: leading_zeroes)
+
+      salt += 1 unless ok
+    end
+
+    salt
+  end
+
+  def read_input
+    File.open(File.expand_path("puzzle.txt", __dir__)).readline.strip
+  end
+
+  def check_head_of(hash:, leading_zeroes:)
+    expected_header = HEAD_MATCHER * leading_zeroes
+
+    hash.start_with? expected_header
+  end
+
+  def hash(input, salt)
+    Digest::MD5.hexdigest(input + salt.to_s)
   end
 end
 
